7f3ff8d0a02147d2da77ff9e5d27943587cebeaa
XDDTech <xddtech@yahoo.com>
2021-08-08 10:57:44 -0400

    Clean up and code refactoring

--- BookAppFunction/src/main/java/mybook/BookApp.java
+++ BookAppFunction/src/main/java/mybook/BookApp.java
@@ -1,14 +1,9 @@
 package mybook;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
@@ -19,30 +14,19 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
 public class BookApp implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
     public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Content-Type", "application/json");
-        headers.put("X-Custom-Header", "application/json");
 
         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
-                .withHeaders(headers);
+                .withHeaders(Utils.getResponseHeaders());
         try {
-            final String pageContents = this.getPageContents("https://checkip.amazonaws.com");
-            String output = String.format("{ \"message\": \"hello world\", \"location\": \"%s\" }", pageContents);
-
+            String output = Utils.getTestOutput();
             return response
                     .withStatusCode(200)
                     .withBody(output);
         } catch (IOException e) {
+
             return response
                     .withBody("{}")
                     .withStatusCode(500);
-        }
-    }
-
-    private String getPageContents(String address) throws IOException{
-        URL url = new URL(address);
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
-            return br.lines().collect(Collectors.joining(System.lineSeparator()));
         }
     }
 }
---
+++ BookAppFunction/src/main/java/mybook/Utils.java
@@ -0,0 +1,28 @@
+package mybook;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Utils {
+    public static Map<String, String> getResponseHeaders() {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        headers.put("X-Custom-Header", "application/json");
+        return headers;
+    }
+
+    public static String getTestOutput() throws IOException {
+        String pageContents = "";
+        URL url = new URL("https://checkip.amazonaws.com");
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
+            pageContents = br.lines().collect(Collectors.joining(System.lineSeparator()));
+        }
+        String output = String.format("{ \"message\": \"hello world\", \"location\": \"%s\" }", pageContents);
+        return output;
+    }
+}
